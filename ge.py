b753def4d7fe075439fa91014f1fde01a50caed4
Cheyenne Goh <prismanox@gmail.com>
2025-01-17 13:37:28 +0000

    Finished adding boilerplate

--- ge.py
+++ ge.py
@@ -6,9 +6,13 @@ from deap import creator, base, tools
 import numpy as np
 import random
 
+import textwrap
+import csv
+
 import warnings
 warnings.filterwarnings("ignore")
 
+# TODO: Write fitness function
 def fitness_eval(individual, points):
     return
 
@@ -49,6 +53,25 @@ CODON_CONSUMPTION = 'lazy'
 GENOME_REPRESENTATION = 'list'
 MAX_GENOME_LENGTH = None
 
+REPORT_ITEMS = ['gen',
+                'invalid',
+                'avg',
+                'std',
+                'min',
+                'max',
+                'fitness_test',
+                'best_ind_length',
+                'avg_length',
+                'best_ind_nodes',
+                'avg_nodes',
+                'best_ind_depth',
+                'avg_depth',
+                'avg_used_codons',
+                'best_ind_used_codons',
+                'structural_diversity',
+                'selection_time',
+                'generation_time']
+
 N_RUNS = 1
 
 for i in range(N_RUNS):
@@ -59,6 +82,10 @@ for i in range(N_RUNS):
     random.seed(RANDOM_SEED)
 
     # TODO: Set dataset and grammar
+    X_train = None
+    Y_train = None
+    X_test = None
+    Y_test = None
     BNF_GRAMMAR = None
 
     population = toolbox.populationCreator(pop_size=POPULATION_SIZE,
@@ -75,3 +102,72 @@ for i in range(N_RUNS):
     stats.register("std", np.nanstd)
     stats.register("min", np.nanmin)
     stats.register("max", np.nanmax)
+
+    population, logbook = algorithms.ge_eaSimpleWithElitism(population,
+                                                            toolbox,
+                                                            cxpb=P_CROSSOVER,
+                                                            ngen=MAX_GENERATIONS,
+                                                            elite_size=ELITE_SIZE,
+                                                            bnf_grammar=BNF_GRAMMAR,
+                                                            codon_size=CODON_SIZE,
+                                                            max_tree_depth=MAX_TREE_DEPTH,
+                                                            max_genome_length=MAX_GENOME_LENGTH,
+                                                            points_train=[X_train, Y_train],
+                                                            points_test=[X_test, Y_test],
+                                                            codon_consumption=CODON_CONSUMPTION,
+                                                            report_items=REPORT_ITEMS,
+                                                            genome_represation=GENOME_REPRESENTATION,
+                                                            stat=stats,
+                                                            halloffame=hof,
+                                                            verbose=False)
+    
+    best = hof.items[0].phenotype
+
+    print("Best individual: \n", "\n".join(textwrap.wrap(best, 80)))
+    print("\nTraining Fitness: ", hof.items[0].fitness.values[0])
+    print("Test Fitness: ", fitness_eval(hof.items[0], [X_test, Y_test])[0])
+    print("Depth: ", hof.items[0].depth)
+    print("Length of the genome: ", len(hof.items[0].genome))
+    print(f"Used portion of the genome: {hof.items[0].used_codons/len(hof.items[0].genome):.2f}")
+
+    max_fitness_values, mean_fitness_values = logbook.select("max", "avg")
+    min_fitness_values, std_fitness_values = logbook.select("min", "std")
+    best_ind_length = logbook.select("best_ind_length")
+    avg_length = logbook.select("avg_length")
+    selection_time = logbook.select("selection_time")
+    generation_time = logbook.select("generation_time")
+    gen, invalid = logbook.select("gen", "invalid")
+    avg_used_codons = logbook.select("avg_used_codons")
+    best_ind_used_codons = logbook.select("best_ind_used_codons")
+    fitness_test = logbook.select("fitness_test")
+    best_ind_nodes = logbook.select("best_ind_nodes")
+    avg_nodes = logbook.select("avg_nodes")
+    best_ind_depth = logbook.select("best_ind_depth")
+    avg_depth = logbook.select("avg_depth")
+    structural_diversity = logbook.select("structural_diversity")
+
+    r = RANDOM_SEED
+    header = REPORT_ITEMS
+
+    with open(r"./results/" + str(r) + ".csv", "w", encoding='UTF8', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter='\t')
+        writer.writerow(header)
+
+        for value in range(len(max_fitness_values)):
+            writer.writerow([gen[value],
+                             invalid[value],
+                             mean_fitness_values[value],
+                             std_fitness_values[value],
+                             min_fitness_values[value],
+                             max_fitness_values[value],
+                             fitness_test[value],
+                             best_ind_length[value],
+                             avg_length[value],
+                             best_ind_nodes[value],
+                             avg_nodes[value],
+                             best_ind_depth[value],
+                             avg_depth[value],
+                             avg_used_codons[value],
+                             structural_diversity[value],
+                             selection_time[value],
+                             generation_time[value]])
